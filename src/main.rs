efb0b4246a659d1d3bb867061e5df2cc89d85492
Dylan Watson <lotsabackscatter@gmail.com>
2023-12-30 13:48:12 +0800

    Kiss3d

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
-use std::io;
-use std::io::prelude::*;
+extern crate kiss3d;
 
-fn pause() {
-    let mut stdin = io::stdin();
-    let mut stdout = io::stdout();
-
-    // We want the cursor to stay at the end of the line, so we print without a newline and flush manually.
-    write!(stdout, "Press any key to continue...").unwrap();
-    stdout.flush().unwrap();
-
-    // Read a single byte and discard
-    let _ = stdin.read(&mut [0u8]).unwrap();
-}
+use kiss3d::nalgebra::{Vector3, UnitQuaternion};
+use kiss3d::window::Window;
+use kiss3d::light::Light;
 
 fn main() {
-    println!("Hello, RG353!");
-    pause();
+    println!("Hello, Kiss3D!");
+
+    let mut window = Window::new("Kiss3d: cube");
+    let mut c      = window.add_cube(1.0, 1.0, 1.0);
+
+    c.set_color(1.0, 0.0, 0.0);
+
+    window.set_light(Light::StickToCamera);
+
+    let rot = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), 0.014);
+
+    while window.render() {
+        c.prepend_to_local_rotation(&rot);
+    }
 }
